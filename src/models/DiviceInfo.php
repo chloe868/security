270dc982d78914d75e451e7b0a14c9c9a11a0d5d
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-06-18 16:00:17 +0800

    Updates

--- src/models/DiviceInfo.php
+++ src/models/DiviceInfo.php
@@ -4,10 +4,12 @@ namespace Increment\Security\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\APIModel;
+
 class DeviceInfo extends APIModel
 {
     protected $table = 'device_info';
     protected $fillable = ['account_id', 'model', 'unique_code', 'details', 'status'];
+    
     public function getAccountIdAttribute($value){
       return intval($value);
     }
